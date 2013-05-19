b0e557da116441c50b6491f54c225baa27a8f430
Nik Tumbri <natumbri@gmail.com>
2013-05-19 22:43:06 +1000

    - remove multiple recipients - send 04xxxxxxxx as 614xxxxxxxx - fix icons, hopefully - remove further reference to mnf, hopefully

---
+++ program/src/com/natumbri/pennytelsms/pennytelsms_help.java
@@ -0,0 +1,17 @@
+package com.natumbri.pennytelsms;
+
+import com.natumbri.pennytelsms.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class pennytelsms_help extends Activity {
+	public static final String SETTINGS_NAME = "MNFSMS-settings";
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+        }
+}
